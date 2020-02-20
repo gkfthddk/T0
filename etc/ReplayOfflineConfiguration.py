a4b7dd60a2612e0d94842c1a904f10fb652932a0
YunJae Lee <gkfthddk@gmail.com>
2020-02-20 14:46:14 +0900

    Update ReplayOfflineConfiguration.py

--- etc/ReplayOfflineConfiguration.py
+++ etc/ReplayOfflineConfiguration.py
@@ -35,7 +35,7 @@ tier0Config = createTier0Config()
 setConfigVersion(tier0Config, "replace with real version")
 
 # Set run number to replay
-setInjectRuns(tier0Config, [ 312851 ])
+setInjectRuns(tier0Config, [ 334393 ])
 print("inject run 327527")
 
 # Settings up sites
